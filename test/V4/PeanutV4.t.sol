de950834f65e1b3026655d98f8bc505253850d28
Alexey Nebolsin <alexey@nebols.in>
2023-12-06 22:59:10 +0400

    Test depositWithAuthorization on Peanut V4.2

--- test/V4/PeanutV4.t.sol
+++ test/V4/PeanutV4.t.sol
@@ -15,7 +15,14 @@ contract PeanutV4Test is Test {
 
     // a dummy private/public keypair to test withdrawals
     address public constant PUBKEY20 = address(0xaBC5211D86a01c2dD50797ba7B5b32e3C1167F9f);
-    bytes32 public constant PRIVKEY = 0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;
+
+    address public constant SAMPLE_ADDRESS = address(0x8fd379246834eac74B8419FfdA202CF8051F7A03);
+    bytes32 public constant SAMPLE_PRIVKEY = 0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;
+
+    // For EIP-3009 testing
+    // keccak256("ReceiveWithAuthorization(address from,address to,uint256 value,uint256 validAfter,uint256 validBefore,bytes32 nonce)")
+    bytes32 public constant RECEIVE_WITH_AUTHORIZATION_TYPEHASH = 0xd099cc98ef71107a616c4f0f941f04c322d8e254fe26b3c6668db87aae413de8;
+    bytes32 public DOMAIN_SEPARATOR = 0xcaa2ce1a5703ccbe253a34eb3166df60a705c561b44b192061e28f2a985be2ca;
 
     function setUp() public {
         console.log("Setting up test");
@@ -61,6 +68,50 @@ contract PeanutV4Test is Test {
         // Test!!!!!!!!
         vm.expectRevert("ECO DEPOSITS MUST USE _contractType 4");
         peanutV4ECO.makeDeposit(address(testToken), 1, 100, 0, address(0));
+    }
+
+    function testMakeDepostERC20Authorization() public {
+        testToken.mint(SAMPLE_ADDRESS, 1000);
+
+        uint256 amount = 1000;
+        bytes32 _nonce = bytes32(0);  // any random value
+        bytes32 authorizationNonce = keccak256(abi.encodePacked(PUBKEY20, _nonce));
+
+        bytes memory typeHashAndData = abi.encode(
+            RECEIVE_WITH_AUTHORIZATION_TYPEHASH,
+            SAMPLE_ADDRESS, // the spender & peanut depositor address
+            address(peanutV4), // receiver of the tokens
+            amount,
+            block.timestamp - 1, // validUntil
+            block.timestamp + 1, // validBefore
+            authorizationNonce
+        );
+
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(typeHashAndData)
+            )
+        );
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(uint256(SAMPLE_PRIVKEY), digest);
+
+        uint256 depositIndex = peanutV4.makeDepositAuthorization(
+            address(testToken),
+            SAMPLE_ADDRESS, // who makes the deposit
+            amount,
+            PUBKEY20,
+            _nonce,
+            block.timestamp - 1, // validUntil
+            block.timestamp + 1, // validBefore
+            v,
+            r,
+            s
+        );
+
+        assertEq(depositIndex, 0, "Deposit failed");
+        assertEq(peanutV4.getDepositCount(), 1, "Deposit count mismatch");
     }
 
     function testMakeDepositERC721() public {
